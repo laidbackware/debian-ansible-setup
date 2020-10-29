c5e12ee64955408dc9d15df0836c7c5c73cfcec9
Matt Proud <proudmatt@gmail.com>
2020-10-29 08:52:31 +0000

    update gnome extensions

--- tasks/desktop/gnome-extensions/update-css.sh
+++ tasks/desktop/gnome-extensions/update-css.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Utility to update Arc Menu and Dash to Panel settings from current system
-cp /home/${USER}/.local/share/gnome-shell/extensions/arc-menu@linxgem33.com/stylesheet.css arc-menu-stylesheet.css
+cp /home/${USER}/.local/share/gnome-shell/extensions/arcmenu@arcmenu.com/stylesheet.css arc-menu-stylesheet.css
 cp /home/${USER}/.local/share/gnome-shell/extensions/dash-to-panel@jderose9.github.com/stylesheet.css dash-to-panel-stylesheet.css
